7e313cf635b16c43e777dd78841062a92a9db7bf
sjf0213 <sjf0213@163.com>
2014-05-04 15:13:39 +0800

    read me

---
+++ SimUV/SimUV/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimUV
+//
+//  Created by song jufeng on 14-5-4.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimUV/SimUV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimUV
+//
+//  Created by song jufeng on 14-5-4.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
