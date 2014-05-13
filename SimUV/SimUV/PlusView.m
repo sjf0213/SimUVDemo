f6f5b19d82e88d8554b1f51f8a7ba68ae332ef9b
sjf0213 <sjf0213@163.com>
2014-05-13 18:25:12 +0800

    Plus view

--- SimUV/SimUV/PlusView.m
+++ SimUV/SimUV/PlusView.m
@@ -15,18 +15,21 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
-        self.backgroundColor = [UIColor greenColor];
+        self.backgroundColor = [UIColor grayColor];
+        NSArray* titleArray = @[@"Drafts", @"Import", @"Camera"];
+        NSArray* imgArray = @[@"uv_createnav_drafts", @"uv_createnav_import", @"uv_createnav_camera"];
+        for (int i = 0; i < 3; i++)
+        {
+            UIButton* btn = [[UIButton alloc] initWithFrame:CGRectMake(0, 75+i*130, frame.size.width, 130)];
+            [btn setImage:[UIImage imageNamed:imgArray[i]] forState:UIControlStateNormal];
+            btn.imageEdgeInsets = UIEdgeInsetsMake(25, 120, 25, 120);
+            [btn setTitle:titleArray[i] forState:UIControlStateNormal];
+            btn.titleEdgeInsets = UIEdgeInsetsMake(105, 0, 0, 80);
+            
+            [self addSubview:btn];
+        }
     }
     return self;
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
