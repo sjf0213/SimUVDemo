876fa0dfc8f70dc8574b84ce9fb322cdad24ee62
sjf0213 <sjf0213@163.com>
2014-05-16 10:05:22 +0800

    Add Core Prj

---
+++ Core/Core/Core.h
@@ -0,0 +1,13 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by song jufeng on 14-5-16.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Core : NSObject
+
+@end
---
+++ Core/Core/Core.m
@@ -0,0 +1,13 @@
+//
+//  Core.m
+//  Core
+//
+//  Created by song jufeng on 14-5-16.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import "Core.h"
+
+@implementation Core
+
+@end
