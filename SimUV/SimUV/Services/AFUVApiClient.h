40ca0ae116519ea98a5dfd99df05d9a8cf0d2cd4
sjf0213 <sjf0213@163.com>
2014-05-16 11:04:07 +0800

    ApiClient

---
+++ SimUV/SimUV/Services/AFUVApiClient.h
@@ -0,0 +1,13 @@
+//
+//  AFUVApiClient.h
+//  SimUV
+//
+//  Created by song jufeng on 14-5-16.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "../../../Core/AFNetworking/AFHTTPSessionManager.h"
+@interface AFUVApiClient : AFHTTPSessionManager
+
+@end
---
+++ SimUV/SimUV/Services/AFUVApiClient.m
@@ -0,0 +1,13 @@
+//
+//  AFUVApiClient.m
+//  SimUV
+//
+//  Created by song jufeng on 14-5-16.
+//  Copyright (c) 2014年 song jufeng. All rights reserved.
+//
+
+#import "AFUVApiClient.h"
+
+@implementation AFUVApiClient
+
+@end
